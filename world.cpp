abe4af2554c52a59d4e0392b96278b3f0aee4b67
unknown <prateekkhalkho@gmail.com>
2022-11-03 14:16:53 +0530

    added cpp file

---
+++ world.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+// void myfunc(string name)
+// {
+//    cout << name << " jenner" << endl;
+// }
+int main()
+{
+   
+   // int pageno =1 ;
+   // while (pageno <= 20)
+   // {
+   //    cout<< "you are at page number-"<<pageno<<endl;
+   // pageno = pageno + 1;
+   // }
+   // int age;
+
+   // cout<<"type your age :";
+   // cin >>age;
+   // if(age>=18)
+   // {
+   //    cout<< "you can vote";
+   // }
+   // else
+   // {
+   //    cout<< "you cannot vote";
+   //    }
+   //
+   // for ( int i = 0; i < 10; i++)
+   // {
+   //    cout<< "value of i is"<<i<<endl;
+   // }
+
+   //creating function
+
+   // myfunc("kendall");
+   // myfunc("kylie");
+
+
+return 0;
+}
